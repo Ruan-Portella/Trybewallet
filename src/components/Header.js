db95edc2989215c0b0aa67cf562f775e349a8002
Ruan-Portella <ruanmorales29@gmail.br>
2023-04-11 15:40:35 -0300

    Feat: Requisito 02 Resolvido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <span data-testid="total-field">0</span>
+        <select data-testid="header-currency-field">
+          <option>BRL</option>
+        </select>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  ...state.user,
+});
+
+export default connect(mapStateToProps)(Header);
--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,24 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+// import { ADD_USER } from '../actions';
+
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+  editor: false,
+  idToEdit: 0,
+};
+
+const walletReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+  case 'RIAN': {
+    return {
+      ...state,
+      email: payload,
+    };
+  }
+  default:
+    return state;
+  }
+};
+
+export default walletReducer;
